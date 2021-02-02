417091454111293fc9aaa42443fbff79b0d195f9
himibaba10 <himibaba10@gmail.com>
2021-02-02 15:01:52 +0600

    null vs undefined cleared

---
+++ undefined-vs-null.js
@@ -0,0 +1,20 @@
+let pakhi; //undefined
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return ;
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const premik = {name: "Smart dude", phone: 01718294}
+// console.log(premik.gf); //undefined
+// const result = add(13);
+// console.log(result);
+// const result = add(13,82);
+// console.log(result);
+// console.log(pakhi);
+
+let fun = undefined; //undefined
+console.log(fun);
